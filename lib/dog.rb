c0861a4cb8764fa8ce2974b5d8b2a43e30f56848
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-26 22:54:57 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+  @@all = []
+
+  attr_accessor :name
+
+  def initialize (name)
+    @name = name
+    @@all << self
+
+  end
+
+  def self.all
+
+    @@all.each do |name|
+      puts "#{name}"
+    end
+
+  end
+
+end
